dd9e976911f2882057e625a13bd650e868832217
vishal <vc8368@gmail.com>
2025-11-07 23:19:22 +0530

    initial setup done

--- index.js
+++ index.js
@@ -11,6 +11,18 @@ app.get( '/', (req ,res)=>{
      })
 })
 
+// app.all("*" , (req, res)=>{
+//      res.status(501).send( {
+//         message:"not implimented"
+//      })
+// })
+
+// app.use( (req, res)=>{
+//      res.status(404).send({
+//          message:"page not found",
+//      })
+// })
+
 app.listen(PORT ,()=>{
     console.log(`server is running on http://localhost:${PORT}`)
 })
